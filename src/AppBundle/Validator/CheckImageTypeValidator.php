3bebdea6aee8075c13da27123154cab686436733
ro8inb <robinberdier@msn.com>
2017-07-31 18:05:41 +0200

    push develop

--- src/AppBundle/Validator/CheckImageTypeValidator.php
+++ src/AppBundle/Validator/CheckImageTypeValidator.php
@@ -8,7 +8,7 @@ use Symfony\Component\Validator\ConstraintValidator;
 class CheckImageTypeValidator extends ConstraintValidator {
 
     public function validate ($value, Constraint $constraint){
-        if($value->getFile() !== null){
+        if($value !== null){
             if($value->getFile()->getmimeType() !== 'image/png' && $value->getFile()->getmimeType() !== 'image/jpeg'){
                $this->context->addViolation($constraint->message);
             }
